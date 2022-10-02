2b3f5983d6b079cc3767674e70c975622f07318a
comp-master-byte <demoniac781@gmail.com>
2022-10-02 16:17:02 +0500

    Все как 3 часа назад

--- src/redux/asyncActions/loginFormAction.js
+++ src/redux/asyncActions/loginFormAction.js
@@ -3,7 +3,7 @@ import { loggedToSystem, submitLoginForm } from '../actionCreators'
 
 export const loginFormAction = (loginData) => {
     return dispatch => {
-        axios.post(`http://localhost:8080/loginn`, {username: loginData.login})
+        axios.post(`http://5.252.21.123:8080/login?username=${loginData.login}&password=${loginData.password}`, {})
             .then(response => {
                 localStorage.setItem('userId', response.data);
                 dispatch(submitLoginForm(response.headers))
